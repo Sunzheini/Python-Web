bb6ee5f4d5867951c51ddc95685e384e86654c44
Sunzheini <daniel_zorov@abv.bg>
2022-09-13 18:55:19 +0300

    Create l1_internet_and_http.py

---
+++ lab/l1_internet_and_http.py
@@ -0,0 +1,55 @@
+# 1. Internet and HTTP
+
+
+# server - tam kydeto stoqt resursite
+# client - koito iska resursite
+
+# network protokol - nachin po koito nie komunikirame mejdu razlichnite device-i
+# htp e network protocol - na principa request / response
+
+# packet - small chunks of data sent over networks
+# header - important info inside packets (contents, origin, destination)
+
+# IP Address - unique id of the device in a specific network
+
+# IPv4
+# 192.168.1.18 - vseki ot komponentite e 8 bita
+# 1 - subnetwork: podmreja
+# 18 - konkreten device
+
+# IPv6 sa poveche komponenti
+
+# DNS - Domain Name Server
+# the human way to access an IP
+# softuni.bg == 217.174.159.34:443
+
+
+# ------------------------------------------------------------------------
+# HTTP - za web
+
+# POST - create / store a result
+# GET - Read / retrieve a resource
+# PUT - Update / modify a resource
+# DELETE - Delete / remove a resource
+
+# these verbs make the CRUD operations
+# Create, Read, Update, Delete
+
+# HEAD - vryshta meradannite
+# OPTIONS - dadeniq klient kakvi prava ima
+
+# -1:37:08
+
+
+
+
+
+
+
+
+
+
+
+
+
+
