f9b4a86ed2604369a079cc78a3300c4da21ca8d1
Sunzheini <daniel_zorov@abv.bg>
2022-09-15 12:41:28 +0300

    Create l2_PostgreSQL_Introduction.py

---
+++ lab/l2_PostgreSQL_Introduction.py
@@ -0,0 +1,38 @@
+# PostgreSQL Introduction
+
+
+# each row is called record or entity
+# columns (fields) define the type opf data they contain
+
+
+# DBMS
+"""
+provides tools
+parses requests
+
+"""
+
+# PostgreSQL - Object - Relational Database management System
+"""
+fastest database
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
