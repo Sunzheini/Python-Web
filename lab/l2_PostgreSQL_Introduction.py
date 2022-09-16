4538853b3e8818fb32e9520054ced6aed7fdeb88
Sunzheini <daniel_zorov@abv.bg>
2022-09-16 13:05:14 +0300

    Update l2_PostgreSQL_Introduction.py

--- lab/l2_PostgreSQL_Introduction.py
+++ lab/l2_PostgreSQL_Introduction.py
@@ -15,14 +15,50 @@ parses requests
 # PostgreSQL - Object - Relational Database management System
 """
 fastest database
-
-
-
+primary key == kato EGN za zapisite v edna tablica
 
 """
 
+# SQL
+"""
+ezik za rabota s danni
+tipove danni: 
+    CHAR(30) - fixed length char
+    VARCHAR(30) - char variable-length with limit. ako nqma () e unlimited length
+    TEXT - string of any length. tyrseneto po text-kolona e bavno
+    SMALLINT, INT, BIGINT
+    DECIMAL, NUMERIC
+    REAL, DOUBLE PRECISION
+    SMALLSERIAL, SERIAL, BIGSERIAL
+
+"""
+
+# Relacii
+"""
+denormalized - nqmame relacii, imame cqlata informaciq na edno qmsto
+    faster, with repeating data
+    
+normalized - nqkolko tablici i vryzka mejdu tqh po kliuch
+    a little bit slower, faster search
+
+foreign key - primary key ot druga tablica v nashata tablica
 
 
+vidove relacii:
+    one to one
+    one to many (defaultna vryzka)
+    many to many - s mejdinna tablica s 2 foreign keys kym drugite 2 tablici
+
+
+ako iskame da iztriem foreign key da vnimavame
+
+"""
+
+# SQL Commands
+"""
+query == zaqvka
+
+"""
 
 
 
