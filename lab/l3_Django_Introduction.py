97fd359abf83fd7e1a49ff3a1e57fa6256ffc7da
Sunzheini <daniel_zorov@abv.bg>
2022-09-20 11:39:33 +0300

    Create l3_Django_Introduction.py

---
+++ lab/l3_Django_Introduction.py
@@ -0,0 +1,40 @@
+# Django Introduction
+
+
+# Framework e next-level biblioteka
+"""
+MV*
+
+- Model contains the data (Database)
+- View handles the UI
+- * handles business logic, gets / sets data into model, return data to view
+
+
+Django izpolzva MTV
+- Model: kato gorniq model
+- Template: kato gornoto view
+- View: kato gornata *
+"""
+
+# Migraciqta ot python class generira SQL zaqvki, koito da se izpylnqt ot database
+# APP oznachava chast ot nashiq dait, koqto e nezavisima ot ostanalite
+# vseki APP si ima MTV i moje da se sydyrja v nqkolko proekta
+
+# porta 8000 moje da go promenim
+# ako sa nqkolko proekta da smenqm porta (ako a pusnati nqkolko)
+# runserver e edinstevo za development celi!
+
+# -------------------------------------------------------------------------------------
+# Terminal v pycharm
+
+"""
+python manage.py startapp task - syzdava papka za app tasks 
+sled tova papkata q premesti v papkata na proekta kydeto e manage.py
+
+
+"""
+
+
+
+
+
