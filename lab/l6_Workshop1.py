0874237d9487e9eef48b7da6d5f1e32f3407381b
Sunzheini <daniel_zorov@abv.bg>
2022-10-03 12:13:02 +0300

    Update l6_Workshop1.py

--- lab/l6_Workshop1.py
+++ lab/l6_Workshop1.py
@@ -77,12 +77,144 @@ urlpatterns = (
 def delete_user(request, pk):
     return render(request, 'accounts/profile-delete-page.html')
 
-another example:
+another example (slugs are also parameters):
 
 def edit_pet(request, username, pet_name):
     return render(request, 'pets/pet-edit-page.html')
 
-13. -1:20
+13. Next to 'templates' create a folder 'staticfiles'
+14. Go to settings.py and add:
+
+STATIC_URL = 'static/'              # files are accesses @ static
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',       # files are located @ staticfiles
+)
+
+15. In each html the path must point to statis/ not staticfiles/ !
+i.e. href="/static/css/styles.css">
+
+16. Combine the common parts: using Template Inheritance:
+    make directory 'base' in 'templates'
+    make base.html
+    put inside the common things, without main, where we replace the contents of main with a block:
+
+<main>
+    {% block page_content %}
+    {% endblock %}
+</main>
+
+    put at the top:
+
+{% extends 'base/base.html' %}
+{% block page_content %}
+
+    put at the botton:
+
+{% endblock %}
+
+    do it for all htmls, except base.html
+    in base.html change:
+
+        href="/static/css/styles.css">
+
+    to:
+
+        href="{% static 'css/styles.css' %}"
+
+    and on top:
+
+        {% load static %}
+
+    the the same with:
+
+        href="/static/images/free-30-instagram-stories-icons23_122570.png"
+        href="{% static 'images/free-30-instagram-stories-icons23_122570.png' %}"
+
+        src="/static/images/free-30-instagram-stories-icons23_122570.png" alt="img1"
+        src="{% static 'images/free-30-instagram-stories-icons23_122570.png'%}"
+                     alt="img1">
+
+    the same with all html files, also press load static
+
+17. In 'base' make folder 'partials' and inside - nav.html
+
+18. Put inside from base.html:
+
+<nav class="navbar">
+    ...
+    </nav>
+
+19. Replace in base.html with following:
+
+{% include 'base/partials/nav.html' %}
+
+20. Where we have:
+
+        <!-- Link to Home Page -->
+        <a href="#">
+            <img width="50px"
+                    src="{% static 'images/free-30-instagram-stories-icons23_122570.png'%}"
+                    alt="img1">
+        </a>
+
+    change:
+
+        <a href="#">
+
+    to:
+
+        <a href="{% url 'index' %}">  # using the names of the urls that we made previously
+
+    if we have parameters, to:
+
+        <a href="{% url 'details user' pk=1 %}">    # currently hardcoding pk
+
+21. Fix all other urls:
+
+    change:
+
+        <a href="#">
+
+    to (for example):
+
+        <a href="{% url 'details user' pk=1 %}">
+
+    on this stage we cannot fix all of them (at a later stage of the workshop)
+
+22. Editing 404.html
+
+in settings.py:
+
+    DEBUG = False
+
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+    ]
+
+but styles are not loaded now, so we type in terminal:
+
+    python manage.py runserver --insecure
+
+then in 404.html:
+
+    {% extends 'base/base.html' %}
+    {%  block page_content %}
+
+    contents of main
+
+    {% endblock %}
+
+    fix the static link and the url link
+
+change back to: DEBUG = True
+
+23.
+
+
+
+
 
 
 
